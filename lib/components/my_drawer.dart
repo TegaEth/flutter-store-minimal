a0bce8682bf9c89f3e647234bf9b9fd8c337ecf2
bunchiochi <ochiojie@gmail.com>
2025-05-21 17:54:05 +0300

    drawer widget made

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/components/my_list_tile.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
@@ -8,19 +9,35 @@ class MyDrawer extends StatelessWidget {
     return Drawer(
       backgroundColor: Theme.of(context).colorScheme.surface,
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           // drawer header: logo
-          DrawerHeader(
+          Column(children: [
+            DrawerHeader(
             child: Center(
               child: Icon(
-                Icons.shopping_bag, 
-                size: 72, 
-                color: Theme.of(context).colorScheme.inversePrimary)),
+                Icons.shopping_bag,
+                size: 72,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
             ),
+          ),
 
-            const SizedBox(height: 25,)
+          const SizedBox(height: 25),
 
           //shop tile
+          MyListTile(text: "Shop", icon: Icons.home, onTap: () {}), 
+          MyListTile(text: "Cart", icon: Icons.shopping_cart, onTap: () {}),
+          ],),
+
+          // exit shop tile
+          Padding(padding: const EdgeInsets.only(bottom: 25.0),
+            child: MyListTile(
+              text: "Exit",
+              icon: Icons.exit_to_app,
+              onTap: () {},
+            ),
+          ),
 
           //cart tile
 
